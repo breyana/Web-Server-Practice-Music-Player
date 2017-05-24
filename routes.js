ed15d72fe2f938c3177a6b9b8a064c415c41b610
Breyana Scales <breyanaj@gmail.com>
2017-05-24 16:03:16 -0700

    songs page renders all songs and info; fixed closing a tag in albums

--- routes.js
+++ routes.js
@@ -3,27 +3,39 @@ const router = express.Router()
 
 const artists = require('./data/artists.json')
 const albums = require('./data/albums.json')
+const songs = require('./data/songs.json')
 
+const albumsWithArtists = albums.map((album) => {
+  album.artist = artists.filter((artist) => {
+    return artist.id === album.artist_id
+  })[0].name
+  return album
+})
+
+const songsWithArtistAlbum = songs.map((song) => {
+  const correspondingAlbum = albumsWithArtists.filter((album) => {
+    return album.id === song.album_id
+  })[0]
+  song.album = correspondingAlbum.title
+  song.artist = correspondingAlbum.artist
+  song.artist_id = correspondingAlbum.artist_id
+  return song
+})
 
 router.get('/', function(request, response) {
   response.render('index', { artists: artists })
 })
 
 router.get('/albums', function(request, response) {
-  //connecting the albums.artist_id with artists
-  const albumsWithArtists = albums.map((album) => {
-    album.artist = artists.filter((artist) => {
-      return artist.id === album.artist_id
-    })[0].name
-    return album
-  })
   response.render('albums', {
     albums: albumsWithArtists
   })
 })
 
 router.get('/songs', function(request, response) {
-  response.render('songs')
+  response.render('songs', {
+    songs: songsWithArtistAlbum
+  })
 })
 
 router.get('/artists/:artist_id', function(request, response) {
